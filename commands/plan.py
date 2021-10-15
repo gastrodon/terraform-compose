26a4cd3a87d7da722f22d06e09664a78bc9cd7d6
Eva <mail@gastrodon.io>
2021-10-15 13:50:53 -0400

    Add a plan command skeleton

--- commands/plan.py
+++ commands/plan.py
@@ -1,6 +1,37 @@
+from typing import List
+
+import typer
+
 import app
+from library import config as lib_config, depends as depends, load
+from library.config import Kind
+from library.types import options
 
 
 @app.app.command()
-def plan():
-    ...
+def plan(
+    services: List[str] = options.services,
+    file: str = options.file,
+):
+    config = load.from_name(file)
+    services = services or config["services"].keys()
+
+    order = [
+        service
+        for group in [
+            depends.order(
+                depends.tree(it, config["services"]),
+                config["services"],
+            )
+            for it in services
+        ]
+        for service in group
+    ]
+
+    configs = {
+        service: lib_config.read(Kind.plan, config["services"][service])
+        for service in order
+    }
+
+    typer.echo(order)
+    typer.echo(configs)
