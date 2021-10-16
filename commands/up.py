cb1e4442dba82b4581b294f47d1d68a3721dbb1a
Eva Harris <mail@gastrodon.io>
2021-10-16 12:17:21 -0400

    Do plan and apply at the same time
    Clean up the argument getting for both, deferring that to functions
    
    Change the usage of terraform.do since it doesn't need the service name

--- commands/up.py
+++ commands/up.py
@@ -1,12 +1,35 @@
-from typing import List
+from os import sys
+from typing import Any, Dict, List
 
 import typer
+from typer import colors
 
 import app
 from library import terraform  # noqa
 from library import config, depends
 from library.types import options
 from library.types.kind import Kind
+
+
+def gather_plan(service: str, compose: Dict[str, Any]):
+    return {
+        "kind": Kind.plan,
+        "args": [],
+        "kwargs": {
+            **config.read(Kind.plan, service, compose["services"][service]),
+            "out": "terraform-compose-tfplan",
+        },
+    }
+
+
+def gather_apply(service: str, compose: Dict[str, Any]):
+    return {
+        "kind": Kind.apply,
+        "args": ["terraform-compose-tfplan"],
+        "kwargs": {
+            **config.read(Kind.apply, service, compose["services"][service]),
+        },
+    }
 
 
 @app.app.command(name="up")
@@ -30,15 +53,28 @@ def handle_up(
     ]
 
     configs = {
-        service: config.read(Kind.plan, service, compose["services"][service])
+        service: {
+            "plan": gather_plan(service, compose),
+            "apply": gather_apply(service, compose),
+        }
         for service in order
     }
 
-    plans = [
-        terraform.do(Kind.plan, key, [], {**value, "out": "terraform-compose-tfplan"})
-        for key, value in configs.items()
-    ]
+    for key, config_set in configs.items():
+        for it in config_set.values():
+            code, stdin, stdout = terraform.do(it["kind"], it["args"], it["kwargs"])
 
-    for it in plans:
-        typer.echo(it[1])
-        typer.echo(it[2])
+            if stdin:
+                typer.secho(stdin, fg=colors.GREEN)
+
+            if stdout:
+                typer.secho(stdout, fg=colors.RED, err=True)
+
+            if code != 0:
+                typer.secho(f"terraform exited with code {code}, continue?")
+
+                try:
+                    if input().lower() != "y":
+                        sys.exit(code)
+                except KeyboardInterrupt:
+                    sys.exit(code)
