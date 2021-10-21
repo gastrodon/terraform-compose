a34beaa87591537e9cf53fd41585abe63ae62f1b
Eva Harris <mail@gastrodon.io>
2021-10-21 17:15:08 -0400

    Refactor dependency getting
    Clean up and rename tree -> dependency tree
    
    Remove order, replaced with order_flat which accepts a collection of
    dependency trees
    
    Add flat_tree and order_levels for flattening dependency trees into
    collections of services which may be brought up at the same time
    
    Remove uniqie [sic], since nothing needs it anymore ( and it was spelled
    wrong! )
    
    Split up depends.py -> depends/{order,tree}.py due to file size and
    relevance

---
+++ library/depends/__init__.py
@@ -0,0 +1,7 @@
+from library.depends.order import order_flat, order_levels  # noqa
+from library.depends.tree import (  # noqa
+    dependency_tree,
+    flat_tree,
+    render_tree,
+    tree_depth,
+)
