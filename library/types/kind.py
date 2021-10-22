869afc79b102c5284e0e247571bb3411bb9e7365
Eva Harris <mail@gastrodon.io>
2021-10-22 09:50:44 -0400

    Add init kind

--- library/types/kind.py
+++ library/types/kind.py
@@ -2,7 +2,7 @@ import enum
 from enum import Enum
 from typing import Dict
 
-from library.config.defaults import APPLY, DESTROY, PLAN
+from library.config.defaults import APPLY, DESTROY, INIT, PLAN
 from library.types.item import Item
 
 
@@ -10,6 +10,7 @@ class Kind(Enum):
     apply = enum.auto()
     destroy = enum.auto()
     plan = enum.auto()
+    init = enum.auto()
 
     @property
     def schema(self) -> Dict[str, Item]:
@@ -17,4 +18,5 @@ class Kind(Enum):
             Kind.apply: APPLY,
             Kind.destroy: DESTROY,
             Kind.plan: PLAN,
+            Kind.init: INIT,
         }[self]
