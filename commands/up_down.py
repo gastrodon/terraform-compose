773763bf3399eb68dc1552053262f5ac261cdce5
Eva Harris <mail@gastrodon.io>
2021-10-21 22:33:38 -0400

    Use do_up for threaded thigns
    Use do_up when mapping config sets to a function in a multiprocessing
    pool, and remove the old function
    
    Each config cluster is a list instead of a dict, with service name
    information inside of the config itself

--- commands/up_down.py
+++ commands/up_down.py
@@ -1,8 +1,6 @@
 from multiprocessing import Pool
 from typing import Any, Dict, List
 
-import typer
-
 import app
 from library import config, depends, terraform
 from library.types import options
@@ -36,29 +34,6 @@ def gather_apply(service: str, compose: Dict[str, Any]):
     }
 
 
-def thread_plan_apply(config: Dict[str, Any]):
-    for action_config in config.values():
-        code, stdout, stderr = terraform.do(
-            action_config["kind"],
-            action_config["args"],
-            action_config["kwargs"],
-        )
-
-        if stdout and code != 0:
-            typer.secho(stdout)
-
-        if stderr and code != 0:
-            typer.secho(stderr, err=True)
-
-        if code != 0:
-            raise Exception(f"terraform exited with code {code}")
-
-
-def do_cluster(configs: List[Dict[str, Any]]) -> List[Any]:
-    with Pool(processes=len(configs)) as pool:
-        return pool.map(thread_plan_apply, configs.values())
-
-
 def do_plan_apply(
     services: List[str] = options.services,
     file: str = options.file,
@@ -67,20 +42,21 @@ def do_plan_apply(
     compose = config.read_file(file)
     services = services or compose["services"].keys()
 
-    config_groups: List[Dict[str, Any]] = [
-        {
-            service: {
+    config_groups: List[List[Dict[str, Any]]] = [
+        [
+            {
+                "service": service,
                 "plan": gather_plan(service, compose, destroy=destroy),
                 "apply": gather_apply(service, compose),
             }
             for service in cluster
-        }
+        ]
         for cluster in gather_services(services, compose)[:: -1 if destroy else 1]
     ]
 
     for group in config_groups:
         with Pool(processes=len(group)) as pool:
-            pool.map(thread_plan_apply, group.values())
+            pool.map(terraform.do_up, group)
 
 
 @app.app.command(name="up")
