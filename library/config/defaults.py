af63051e073a9e3379bf1879936bd98e85c4adf8
Eva Harris <mail@gastrodon.io>
2021-10-22 10:36:14 -0400

    Reuse path config item

--- library/config/defaults.py
+++ library/config/defaults.py
@@ -2,24 +2,29 @@ from typing import Dict, List, Optional
 
 from library.types.item import Item
 
+shared: Dict[str, Item] = {
+    "path": Item(None, True, str),
+}
+
 APPLY: Dict[str, Item] = {
+    **shared,
     "input": Item(False, False, bool),
     "lock": Item(False, False, bool),
     "parallelism": Item(10, False, int),
-    "path": Item(None, True, Optional[str]),
     "state-backup": Item(None, False, Optional[str]),
     "state-out": Item(None, False, Optional[str]),
     "state": Item(None, False, Optional[str]),
 }
 
 DESTROY: Dict[str, Item] = {
+    **shared,
     **APPLY,
     "no-destroy": Item(False, False, bool),
 }
 
 PLAN: Dict[str, Item] = {
+    **shared,
     "input": Item(False, False, bool),
-    "path": Item(None, True, str),
     "refresh-only": Item(False, False, bool),
     "refresh": Item(False, False, bool),
     "replace": Item(None, False, Optional[str]),
@@ -29,18 +34,18 @@ PLAN: Dict[str, Item] = {
 }
 
 INIT: Dict[str, Item] = {
+    **shared,
     "backend-config": Item([], False, List[str]),
     "backend": Item(True, False, bool),
     "force-copy": Item(False, False, bool),
     "get": Item(True, False, bool),
     "input": Item(False, False, bool),
     "migrate-state": Item(False, False, bool),
-    "path": Item(None, True, str),
     "plugin-dir": Item([], False, List[str]),
     "reconfigure": Item(False, False, bool),
     "upgrade": Item(False, False, bool),
 }
 
 OUTPUT: Dict[str, Item] = {
-    "path": Item(None, True, str),
+    **shared,
 }
