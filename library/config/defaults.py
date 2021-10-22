fcb0a6c2d545d44e08b64724d48a9cb699c1b415
Eva Harris <mail@gastrodon.io>
2021-10-22 02:00:15 -0400

    Create INIT config schema
    Add input flag to other config items, with a false default

--- library/config/defaults.py
+++ library/config/defaults.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Optional
 from library.types.item import Item
 
 APPLY: Dict[str, Item] = {
+    "input": Item(False, False, bool),
     "lock": Item(False, False, bool),
     "parallelism": Item(10, False, int),
     "path": Item(None, True, Optional[str]),
@@ -17,6 +18,7 @@ DESTROY: Dict[str, Item] = {
 }
 
 PLAN: Dict[str, Item] = {
+    "input": Item(False, False, bool),
     "path": Item(None, True, str),
     "refresh-only": Item(False, False, bool),
     "refresh": Item(False, False, bool),
@@ -24,4 +26,16 @@ PLAN: Dict[str, Item] = {
     "target": Item(None, False, Optional[str]),
     "var-files": Item([], False, List[str]),
     "vars": Item(dict(), False, Dict[str, str]),
+}
+
+INIT: Dict[str, Item] = {
+    "backend-config": Item([], False, List[str]),
+    "backend": Item(True, False, bool),
+    "force-copy": Item(False, False, bool),
+    "get": Item(True, False, bool),
+    "input": Item(False, False, bool),
+    "migrate-state": Item(False, False, bool),
+    "plugin-dir": Item([], False, List[str]),
+    "reconfigure": Item(False, False, bool),
+    "upgrade": Item(False, False, bool),
 }
