bbd412fe6ab33fe560b971809f85f63d3a46ef63
Eva <mail@gastrodon.io>
2022-03-22 07:47:46 -0400

    Pass path and file separately to load
    Also, leave a TODO

--- library/load/__init__.py
+++ library/load/__init__.py
@@ -1,14 +1,18 @@
+import os
+
 import yaml
 
 import library.load.handle  # noqa
 from library.load.route import parse
 from library.model import Compose, compose
 
-PATH_COMPOSE = "./terraform-compose.yml"
+COMPOSE_FILE = "terraform-compose.yml"
 
 
-def load(args: ..., path: str = PATH_COMPOSE) -> Compose:
-    with open(path) as stream:
+def load(args: ..., path: str = ".", file: str = COMPOSE_FILE) -> Compose:
+    # TODO better path / file exists / is valid checking
+
+    with open(os.path.join(path, file)) as stream:
         parsed = parse.parse(None, args, yaml.safe_load(stream.read()))
 
     return compose.from_dict(parsed)
