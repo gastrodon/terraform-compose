d4abd99ea2c8415b71033e7e623ca5a2841467ea
Eva <mail@gastrodon.io>
2022-03-26 18:21:08 -0400

    Simplify compose loading

--- library/model/compose.py
+++ library/model/compose.py
@@ -11,14 +11,5 @@ class Compose:
     services: Dict[str, Config] = field(default_factory=dict)
 
 
-def from_file(path: str) -> Compose:
-    with open(path) as stream:
-        return from_raw(path.read())
-
-
-def from_raw(compose: str) -> Compose:
-    return from_dict(yaml.safe_load(compose))
-
-
 def from_dict(compose: Dict) -> Compose:
     return dacite.from_dict(data=compose, data_class=Compose)
