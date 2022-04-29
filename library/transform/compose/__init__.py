764eb508eaa4b73dda9938ffe76948b5ddf69582
Eva <mail@gastrodon.io>
2022-04-29 14:52:47 -0400

    Remove widen

--- library/transform/compose/__init__.py
+++ library/transform/compose/__init__.py
@@ -1,4 +1,3 @@
 from library.transform.compose.find import find  # noqa
 from library.transform.compose.flatten import flatten  # noqa
 from library.transform.compose.merge import merge  # noqa
-from library.transform.compose.widen import widen  # noqa
