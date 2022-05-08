c93aa4ce18fda9b1b4b4cdf7216545d7983110bb
Eva <mail@gastrodon.io>
2022-05-08 14:59:16 -0400

    Export argument models

--- library/model/cli/__init__.py
+++ library/model/cli/__init__.py
@@ -1,2 +1,8 @@
+from library.model.cli.argument import (  # noqa
+    Argument,
+    ArgumentCommand,
+    ArgumentFlag,
+    ArgumentSeparator,
+)
 from library.model.cli.kind import ArgumentKind  # noqa
 from library.model.cli.scope import ArgumentScope  # noqa
