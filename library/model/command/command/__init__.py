d62d867aa6ae17e3e0892f39ad74f3cb06f42bb9
Eva <mail@gastrodon.io>
2022-05-08 15:09:58 -0400

    Export Terraform command

--- library/model/command/command/__init__.py
+++ library/model/command/command/__init__.py
@@ -1,5 +1,6 @@
 from library.model.command.command.graph import Graph  # noqa
 from library.model.command.command.init import Init  # noqa
 from library.model.command.command.show import Show  # noqa
+from library.model.command.command.terraform import Terraform  # noqa
 from library.model.command.command.up_down import Down, Up  # noqa
 from library.model.command.command.validate import Validate  # noqa
