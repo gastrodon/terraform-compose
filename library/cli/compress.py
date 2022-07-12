d73b5f401d7e639bfe0f0491a83c917296d6959b
Eva <mail@gastrodon.io>
2022-07-12 17:40:29 -0400

    Compress dicty data

--- library/cli/compress.py
+++ library/cli/compress.py
@@ -1,19 +1,21 @@
 from typing import List
 
-
 from library.model.cli import ArgumentScope
-from library.model.cli.argument import (
-    Argument,
-    ArgumentKV,
-)
+from library.model.cli.argument import Argument, ArgumentKV
 
 
 def compress(arguments: List[Argument], scope: ArgumentScope) -> List[Argument]:
-    arg_filter = lambda it: isinstance(it.value, list) and it.scope == scope
-    compressed = {argument.key: [] for argument in filter(arg_filter, arguments)}
+    arg_filter = lambda it: isinstance(it.value, (list, dict)) and it.scope == scope
+    compressed = {
+        argument.key: [] if isinstance(argument.value, list) else {}
+        for argument in filter(arg_filter, arguments)
+    }
 
     for argument in filter(arg_filter, arguments):
-        compressed[argument.key] += argument.value
+        if isinstance(argument.value, list):
+            compressed[argument.key] += argument.value
+        elif isinstance(argument.value, dict):
+            compressed[argument.key] = {**compressed[argument.key], **argument.value}
 
     return [
         *filter(lambda it: not arg_filter(it), arguments),
