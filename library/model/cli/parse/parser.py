88b8cb3b552c7c0545a2508df0bcf109139b8e1b
Eva <mail@gastrodon.io>
2022-07-15 07:27:23 -0400

    Don't use TerraformCommand class

--- library/model/cli/parse/parser.py
+++ library/model/cli/parse/parser.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from typing import Any, Callable, Set
 
 from library.model.cli.kind import ArgumentKind
-from library.model.terraform import TerraformCommand
 
 
 @dataclass
@@ -10,7 +9,7 @@ class ArgumentParser:
     kind: ArgumentKind
     default: Any = None
     rename: str = None
-    commands: Set[TerraformCommand] = field(default_factory=set)
+    commands: Set[str] = field(default_factory=set)
     parser: Callable[[Any], Any] = lambda it: it
 
     def parse(self, data: Any):
