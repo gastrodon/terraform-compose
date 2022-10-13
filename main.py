c085a51197f7370250e106eed721a5222d5dc0a3
Eva <mail@gastrodon.io>
2022-10-13 10:56:24 -0400

    Use freeze_support to fix pyinstaller build

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+from multiprocessing import Process, freeze_support
+
 import app
 import commands  # noqa
 
 if __name__ == "__main__":
-    app.app()
+    freeze_support()
+    Process(target=app.app).start()
